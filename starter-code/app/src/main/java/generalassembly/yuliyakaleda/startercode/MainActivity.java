aaa76d54da9a47db87780ede571c273199c2fde3
Yanni-Psitos <ypsitos@gmail.com>
2016-02-19 17:20:04 -0500

    Yanni Psitos - Complete W/ Bonus

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -5,23 +5,58 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+  ArrayList<String> mWishList;
+  ArrayAdapter<String> mAdapter;
+  ListView wishListOut;
+  TextView wishOut;
+  EditText userInput;
+  Button wishButton;
+  Animation mSpinIn;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    //TODO: set up all the view and event listeners.
-  }
-
-  @Override
-  public void onClick(View v) {
-    // TODO: 1. get the text from the input field
-    //       2. animate it in the center of the screen
-    //       3. add it to the list wish
-    //       4. clear the input field
+    mSpinIn = AnimationUtils.loadAnimation(MainActivity.this,R.anim.animation);
+    wishListOut = (ListView)findViewById(R.id.wishListView);
+    mWishList = new ArrayList<String>();
+    mAdapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, mWishList);
+    wishListOut.setAdapter(mAdapter);
+    wishOut = (TextView) findViewById(R.id.wishOutTv);
+    userInput = (EditText) findViewById(R.id.userInput);
+    wishButton = (Button) findViewById(R.id.wishMakerButton);
+    wishButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        wishOut.setText(userInput.getText());
+        wishOut.startAnimation(mSpinIn);
+        mWishList.add(userInput.getText().toString());
+        mAdapter.notifyDataSetChanged();
+        userInput.setText("");
+      }
+    });
+    wishListOut.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        mWishList.remove(position);
+        mAdapter.notifyDataSetChanged();
+      }
+    });
   }
 }
